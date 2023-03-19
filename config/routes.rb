3248a6eb5788603d30fe426b7a4f52d4cb708334
shunsuke0424 <shunsuke603ikuno@gmail.com>
2023-03-19 19:39:59 +0900

    Add view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,46 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    post "users/destroy/:id" => "users#destroy"
+    post "users/update/:id" => "users#update"
+    get "users/index" => "users#index"
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # user
+  post "users/create" => "users#create"
+  post "users/destroy/:id" => "users#destroy"
+  get "signup" => "users#new"
+  # get "users/index" => "users#index"
+  get "users/:id" => "users#show"
+  post "login" => "users#login"
+  post "logout" => "users#logout"
+  # post "create_guest_user" => "users#create_guest_user"
+  # get "create_guest_user" => "users#create_guest_user"
+  get "login" => "users#login_form"
+  get "users/ranking/:id" => "users#ranking"
+  get "detail" => "users#detail"
+
+  # group
+  post "groups/create" => "groups#create"
+  post "groups/join" => "groups#join"
+  post "groups/signup_join/:id_token" => "groups#signup_join"
+  post "groups/login_join/:id_token" => "groups#login_join"
+  get "groups/index" => "groups#index"
+  get "groups/:id_token" => "groups#show"
+  get "create_group" => "groups#create_form"
+  get "join_group" => "groups#join_form"
+  get "/signup_join_form/:id_token" => "groups#signup_join_form"
+  get "/login_join_form/:id_token" => "groups#login_join_form"
+  get "/invite/:id_token" => "groups#invite"
+  
+  # vote
+  post "votes/:keyword_id/:voter_id/:voted_id/create" => "votes#create"
+  post "votes/:keyword_id/:voter_id/:voted_id/destroy" => "votes#destroy"
+
+  # like
+  post "likes/:liked_user_id/create" => "likes#create"
+  post "likes/:liked_user_id/destroy" => "likes#destroy"
+
+  # home
+  get "/" => "home#about"
 end
+
